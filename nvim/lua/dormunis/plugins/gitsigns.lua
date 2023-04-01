d0d04fb3d40f218d688c16fa4a594accb282c9b1
Dor Munis <dormunis@gmail.com>
2023-04-02 00:44:50 +0300

    gitsigns bugfix

--- nvim/lua/dormunis/plugins/gitsigns.lua
+++ nvim/lua/dormunis/plugins/gitsigns.lua
@@ -26,7 +26,7 @@ return {
                 delay = 1000,
                 ignore_whitespace = false,
             },
-            currnet_line_blame_formatter = '<author>, <author_time:%Y-%m-%d - <summary>',
+            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d - <summary>',
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
