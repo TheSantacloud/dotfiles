b4d574fa1767c6f497c72103c95c042c47bd4f08
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-04-04 16:58:39 +0300

    Added open-file-to-nvim functionality

--- zsh/aliases/customized.plugin.zsh
+++ zsh/aliases/customized.plugin.zsh
@@ -2,3 +2,4 @@ alias ls="ls -h --color=auto"
 alias ll="ls -lah"
 alias vim=nvim
 alias aliases="vim $ZSH/aliases/customized.plugin.zsh"
+alias v="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
