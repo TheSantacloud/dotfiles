1180251e4128c8e8a6462eb94673f8857ffccea5
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-04-08 16:19:24 +0300

    minor lsp keybinding change

--- nvim/lua/dormunis/lspopts.lua
+++ nvim/lua/dormunis/lspopts.lua
@@ -113,9 +113,9 @@ cmp.setup {
             select = true,
         },
         ['<Tab>'] = cmp.mapping(function(fallback)
-            -- if cmp.visible() then
-            --     cmp.select_next_item()
-            if luasnip.expand_or_jumpable() then
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
             else
                 fallback()
