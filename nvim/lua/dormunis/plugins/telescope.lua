b1b70255f5532788cf65b9b8b26158b79598f47b
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-04-11 21:53:12 +0200

    fuzzy in buffer

--- nvim/lua/dormunis/plugins/telescope.lua
+++ nvim/lua/dormunis/plugins/telescope.lua
@@ -10,13 +10,14 @@ return {
         end, { desc = 'File actions' })
         vim.keymap.set('n', '<leader>f?', require('telescope.builtin').oldfiles,
             { desc = '[?] Find recently opened files' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
+        vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = '[F]ind in [C]urrent buffer' })
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles from project root' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind [G]rep on all files from project root' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp - fuzzy search help' })
-        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = '[C-p] Find files in git repo' })
         vim.keymap.set('n', '<leader>fw', function()
             builtin.grep_string({ search = vim.fn.expand('<cword>') })
         end, { desc = '[F]ind [W]ord - fuzzy search word under cursor' })
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = '[C-p] Find files in git repo' })
     end,
 }
