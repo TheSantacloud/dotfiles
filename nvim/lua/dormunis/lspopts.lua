3a6462762fccab56d6a47221db2a0c5ca7edfde1
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-05-15 18:00:18 +0300

    removed irrelevant comments

--- nvim/lua/dormunis/lspopts.lua
+++ nvim/lua/dormunis/lspopts.lua
@@ -1,12 +1,4 @@
--- LSP settings.
---  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself
-    -- many times.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
     local nmap = function(keys, func, desc)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
@@ -23,11 +15,9 @@ local on_attach = function(_, bufnr)
     nmap('<leader>csd', require('telescope.builtin').lsp_document_symbols, '[C]ode [S]ymbols: [D]ocument')
     nmap('<leader>csw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[C]ode [S]ymbols: [W]orkspace')
 
-    -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-    -- Lesser used LSP functionality
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('<leader>w', function()
     end, 'Workspace')
@@ -37,16 +27,12 @@ local on_attach = function(_, bufnr)
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, '[w]orkspace [l]ist folders')
 
-    -- Set formatting with keybinding
     nmap("<leader>cf", function()
             vim.lsp.buf.format()
         end,
         '[C]ode [F]ormat file')
 end
 
--- Enable the following language servers
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
     clangd        = {},
     gopls         = {},
@@ -64,17 +50,13 @@ local servers = {
     },
 }
 
--- Setup neovim lua configuration
 require('neodev').setup()
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- Setup mason so it can manage external tooling
 require('mason').setup()
 
--- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
@@ -91,7 +73,6 @@ mason_lspconfig.setup_handlers {
     end,
 }
 
--- nvim-cmp setup
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 
