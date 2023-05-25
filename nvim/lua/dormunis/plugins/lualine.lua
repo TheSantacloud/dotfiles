3e75ae8fd03e5f5d6e3cd6e47fa007990ebb0904
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-05-25 17:07:49 +0300

    Switch lualine theme to dracula

--- nvim/lua/dormunis/plugins/lualine.lua
+++ nvim/lua/dormunis/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'gruvbox',
+                theme = 'dracula',
             },
             sections = {
                 lualine_a = {
