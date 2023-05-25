6903ab321eba101862fb864971a9b0fa700dda87
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-05-25 17:06:50 +0300

    Removed irrelevant keybindings

--- nvim/lua/dormunis/keymap.lua
+++ nvim/lua/dormunis/keymap.lua
@@ -35,13 +35,13 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-
+-- vim.keymap.set("n", "<leader>y", "\"+y")
+-- vim.keymap.set("v", "<leader>y", "\"+y")
+-- vim.keymap.set("n", "<leader>Y", "\"+Y")
+--
+-- vim.keymap.set("n", "<leader>d", "\"_d")
+-- vim.keymap.set("v", "<leader>d", "\"_d")
+--
 -- B to go to the beginning of the line (start of the first word)
 vim.keymap.set("n", "B", "^")
 -- E to get to the end of the line
