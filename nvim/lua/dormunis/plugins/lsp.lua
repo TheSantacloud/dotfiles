337e1881b8f6635aa1753bb55e364110212f73bb
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-08-02 17:43:57 +0300

    Fixing a deprecated something

--- nvim/lua/dormunis/plugins/lsp.lua
+++ nvim/lua/dormunis/plugins/lsp.lua
@@ -4,8 +4,8 @@ return {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
 
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        { 'j-hui/fidget.nvim', opts = {} },
+        -- -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+        -- { 'j-hui/fidget.nvim', opts = {} },
 
         'folke/neodev.nvim',
     },
