9bebc18f3255e0985366ff56adfa31fd47528bc6
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-10-05 16:06:20 +0300

    some fugitive updates

--- nvim/lua/dormunis/plugins/fugitive.lua
+++ nvim/lua/dormunis/plugins/fugitive.lua
@@ -3,6 +3,20 @@ return {
     event = "VeryLazy",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = 'Git status' })
-        vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git({ 'pull' }) end, { desc = 'Git pull' })
+        vim.keymap.set("n", "<leader>gpl", function() vim.cmd.Git({ 'pull' }) end, { desc = 'Git pull' })
+        vim.keymap.set("n", "<leader>ga", function()
+            local bufnr = vim.api.nvim_get_current_buf();
+            local filepath = vim.api.nvim_buf_get_name(bufnr);
+            vim.cmd('Git add ' .. filepath)
+            vim.notify('Git added: ' .. filepath)
+        end, { desc = 'Git add' })
+        vim.keymap.set("n", "<leader>gcm", function()
+            local message = vim.fn.input('Commit message: ')
+            if message ~= '' then
+                vim.cmd('Git commit -m "' .. message .. '"')
+            else
+                vim.notify('Commit message cannot be empty', vim.log.levels.ERROR)
+            end
+        end, { desc = 'Git commit' })
     end
 }
