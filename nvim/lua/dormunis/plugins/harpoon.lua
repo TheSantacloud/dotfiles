39513330021dd270472385c9b0e6b2b7592585de
Dor Munis <5811812+dormunis@users.noreply.github.com>
2023-12-30 22:59:43 +0200

    removed irrelevat plugins

--- nvim/lua/dormunis/plugins/harpoon.lua
+++ nvim/lua/dormunis/plugins/harpoon.lua
@@ -7,25 +7,12 @@ return {
     },
     config = function()
         local harpoon = require('harpoon')
-        harpoon:setup({})
-
-        -- configure telescope
-        -- local conf = require("telescope.config").values
-        -- local function toggle_telescope(harpoon_files)
-        --     local file_paths = {}
-        --     for _, item in ipairs(harpoon_files.items) do
-        --         table.insert(file_paths, item.value)
-        --     end
-        --
-        --     require("telescope.pickers").new({}, {
-        --         prompt_title = "Harpoon",
-        --         finder = require("telescope.finders").new_table({
-        --             results = file_paths,
-        --         }),
-        --         previewer = conf.file_previewer({}),
-        --         sorter = conf.generic_sorter({}),
-        --     }):find()
-        -- end
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+                save_on_change = true,
+            },
+        })
 
         vim.keymap.set("n", "<leader>fh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
             { desc = "Open harpoon window" })
