0579463e5f82f474aae0e87785598d34104b05d2
Dor Munis <5811812+dormunis@users.noreply.github.com>
2024-01-04 20:49:14 +0200

    chore: changed to gruvbox-material

--- nvim/lua/dormunis/plugins/colorscheme.lua
+++ nvim/lua/dormunis/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
-    "savq/melange-nvim",
+    "sainnhe/gruvbox-material",
     config = function()
         vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
-        vim.cmd("colorscheme melange")
+        vim.cmd("let g:gruvbox_material_background = 'medium'")
+        vim.cmd("let g:gruvbox_material_better_performance = 1")
+        vim.cmd("let g:gruvbox_material_transparent_background = 2")
+        vim.cmd("let g:gruvbox_material_dim_inactive_windows = 1")
+        vim.cmd("let g:gruvbox_material_float_style = 'dim'")
+        vim.cmd("colorscheme gruvbox-material")
     end,
 }
