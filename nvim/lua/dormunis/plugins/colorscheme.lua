35a41668d15c8fd688c437b5d2afaa726f7a555d
Dor Munis <5811812+dormunis@users.noreply.github.com>
2024-02-01 11:39:25 +0200

    changed colorscheme to rose pine

--- nvim/lua/dormunis/plugins/colorscheme.lua
+++ nvim/lua/dormunis/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
+-- return {
+--     "ellisonleao/gruvbox.nvim",
+--     config = function()
+--         vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+--         require("gruvbox").setup({
+--             transparent_mode = true
+--         })
+--         vim.cmd("colorscheme gruvbox")
+--     end,
+-- }
 return {
-    "ellisonleao/gruvbox.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-        vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
-        require("gruvbox").setup({
-            transparent_mode = true
+        require('rose-pine').setup({
+            dim_inactive_windows = false,
+            styles = {
+                transparency = 0.8,
+            }
         })
-        vim.cmd("colorscheme gruvbox")
-    end,
+        vim.cmd("colorscheme rose-pine")
+    end
 }
