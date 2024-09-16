0d5f34794102b9352934f43feda7857b53c3834c
Dor Munis <5811812+dormunis@users.noreply.github.com>
2024-09-16 18:45:59 +0300

    feat: jupyter notebook

--- zsh/aliases/customized.plugin.zsh
+++ zsh/aliases/customized.plugin.zsh
@@ -4,3 +4,6 @@ alias vim=nvim
 alias lvim="nvim -c':e#<1'"
 alias a="alias | fzf"
 alias s="poetry shell"
+alias jn="source ~/Library/Caches/pypoetry/virtualenvs/jupyter/bin/activate && jupyter notebook --notebook-dir='~/dev/jupyter-notebooks' --port=18733 > ~/jupyter.log 2>&1 &"
+alias jk="pgrep -f 'jupyter' | xargs kill"
+
