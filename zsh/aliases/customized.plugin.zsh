893cbde3ec17010504d6690d99fdce3902ead0f5
Dor Munis <5811812+dormunis@users.noreply.github.com>
2024-09-17 08:56:19 +0300

    fix: singleton jn

--- zsh/aliases/customized.plugin.zsh
+++ zsh/aliases/customized.plugin.zsh
@@ -4,6 +4,12 @@ alias vim=nvim
 alias lvim="nvim -c':e#<1'"
 alias a="alias | fzf"
 alias s="poetry shell"
-alias jn="source ~/Library/Caches/pypoetry/virtualenvs/jupyter/bin/activate && jupyter notebook --notebook-dir='~/dev/jupyter-notebooks' --port=18733 > ~/jupyter.log 2>&1 &"
 alias jk="pgrep -f 'jupyter' | xargs kill"
+alias jn='if lsof -i:18733 > /dev/null; then \
+  open "http://localhost:18733"; \
+else \
+  source ~/Library/Caches/pypoetry/virtualenvs/jupyter/bin/activate && \
+  jupyter notebook --notebook-dir="~/dev/jupyter-notebooks" --port=18733 > ~/jupyter.log 2>&1 & \
+fi'
+
 
