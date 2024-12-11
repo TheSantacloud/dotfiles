444a9cfcfec59066c9a464e8f3f45af3539d2dd6
Dor Munis <5811812+dormunis@users.noreply.github.com>
2024-12-11 15:24:34 +0200

    using mini rather than lualine

--- nvim/lua/dormunis/plugins/hud.lua
+++ nvim/lua/dormunis/plugins/hud.lua
@@ -1,54 +1,17 @@
 return {
     {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+        'echasnovski/mini.nvim',
+        version = "*",
         config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "auto",
-                    fmt = string.lower,
-                },
-                sections = {
-                    lualine_a = { "branch" },
-                    lualine_b = {
-                        {
-                            "filename",
-                            path = 1,
-                        }
-                    },
-                    lualine_c = {
-                        {
-                            "diagnostics",
-                            sources = { "nvim_diagnostic", "coc" },
-                            sections = { "error", "warn", "info", "hint" },
-                            diagnostics_color = {
-                                error = "DiagnosticError",
-                                warn = "DiagnosticWarn",
-                                info = "DiagnosticInfo",
-                                hint = "DiagnosticHint",
-                            },
-                            symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-                            colored = true,
-                            update_in_insert = false,
-                            always_visible = false,
-                        },
-                    },
-                    lualine_x = { "encoding", "fileformat", "filetype" },
-                    lualine_y = { "progress" },
-                    lualine_z = { "location" },
-                },
-            })
-        end,
+            local statusline = require("mini.statusline")
+            statusline.setup({ use_icons = true })
+        end
     },
     {
         "folke/which-key.nvim",
         dependencies = { "echasnovski/mini.icons" },
         event = "VeryLazy",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
+        opts = {},
         keys = {
             {
                 "<leader>?",
