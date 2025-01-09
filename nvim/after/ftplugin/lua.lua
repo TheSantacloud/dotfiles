df5c201faf030fb040a7afff84b4055114d3b793
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-01-09 18:24:44 +0200

    made some convenient python run commands

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,5 @@
+vim.bo.shiftwidth = 2
+
+vim.keymap.set("n", "<space><space>x", ":source %<CR>")
+vim.keymap.set("n", "<space><space>r", ":.lua<CR>")
+vim.keymap.set("v", "<space><space>r", ":.lua<CR>")
