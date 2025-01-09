ec7d999c4ed6defa95a79a5e64f1219448024d71
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-01-09 18:24:44 +0200

    made some convenient python run commands

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,7 +1,62 @@
-vim.api.nvim_buf_set_keymap(
-    0,
-    'n',
-    '<space><space>r',
-    [[:VimuxRunCommand "python ]] .. vim.fn.expand('%:p') .. [["<CR>]],
-    { noremap = true, silent = true }
-)
+--- Gets the selected code
+--- @return string "Code Snippet"
+local function get_selected_code()
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+  local lines = vim.fn.getline(start_line, end_line)
+  if type(selected_lines) == "string" then
+    selected_lines = { selected_lines }
+  end
+  return table.concat(lines, "\n")
+end
+
+--- Gets the relevant python binary path (also considering virtual environments)
+--- @return string "Python binary path"
+local function get_python_bin()
+  local virtual_env = os.getenv("VIRTUAL_ENV")
+  local python_bin = "python"
+  if virtual_env ~= nil and string.find(virtual_env, "poetry") then
+    python_bin = virtual_env .. "/bin/python"
+  end
+  return python_bin
+end
+
+--- Executes a python snippet to messages within a specified binary
+--- @param code string Code
+--- @param opts table|nil Options table (optional). Must include `bin` (string) and `echo` (boolean).
+local function execute_python_snippet(code, opts)
+  opts = opts or {}
+  local bin = opts.bin or "python"
+  local echo = opts.echo or true
+
+  local output = vim.fn.system({ bin, '-c', code })
+  if vim.v.shell_error == 0 and echo then
+    vim.api.nvim_echo({ { output, "Normal" } }, false, {})
+  else
+    vim.api.nvim_echo({ { "Error: " .. output, "ErrorMsg" } }, false, {})
+  end
+end
+
+local function prep_poetry_session()
+  if vim.fn.executable("poetry") ~= 1 or
+      vim.loop.fs_stat(vim.loop.cwd() .. "/" .. "poetry.lock") == nil then
+    return
+  end
+  vim.cmd([[VimuxRunCommand "\[\[ $VIRTUAL_ENV != *poetry* \]\] && poetry shell"]])
+end
+
+local function run_python_file(path)
+  prep_poetry_session()
+  vim.cmd('VimuxRunCommand "python ' .. path .. '"')
+end
+
+vim.keymap.set('n', '<space><space>r', function()
+  local filepath = vim.fn.expand('%:p')
+  run_python_file(filepath)
+end)
+
+vim.keymap.set('v', '<space><space>r', function()
+  local code = get_selected_code()
+  local python_bin = get_python_bin()
+  execute_python_snippet(code, { bin = python_bin })
+end)
--- nvim/lua/dormunis/plugins/lsp.lua
+++ nvim/lua/dormunis/plugins/lsp.lua
@@ -1,198 +1,205 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        { "williamboman/mason.nvim", config = true },
-        { "j-hui/fidget.nvim",       opts = {} },
-        { 'saghen/blink.cmp' },
-        "williamboman/mason-lspconfig.nvim",
-        "folke/neodev.nvim",
-        "nvimtools/none-ls.nvim",
-    },
-    config = function()
-        local on_attach = function(_, bufnr)
-            local nmap = function(keys, func, desc)
-                vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-            end
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", config = true },
+    { "j-hui/fidget.nvim",       opts = {} },
+    { 'saghen/blink.cmp' },
+    "williamboman/mason-lspconfig.nvim",
+    "folke/neodev.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function()
+    local on_attach = function(_, bufnr)
+      local nmap = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+      end
 
-            nmap("<leader>r", vim.lsp.buf.rename, "Rename")
-            nmap("gD", vim.lsp.buf.declaration, "Go to declaration")
-            nmap("gd", vim.lsp.buf.definition, "Go to definition")
-            nmap("gR", require("telescope.builtin").lsp_references, "Get references in a telescope list")
-            nmap("gr", function()
-                vim.lsp.buf.references(nil, {
-                    on_list = function(options)
-                        vim.fn.setqflist({}, " ", options)
-                        vim.api.nvim_command("cfirst")
-                    end,
-                })
-            end, "Get references in a quickfix list")
-            nmap("gI", vim.lsp.buf.implementation, "Go to implementation")
-            nmap("gt", vim.lsp.buf.type_definition, "Go to type definition")
-            nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-            nmap("H", vim.lsp.buf.signature_help, "Signature Help")
-            nmap("<leader>cf", vim.lsp.buf.format, "Format file")
-            nmap("<leader>ca", vim.lsp.buf.code_action, "Code action")
-            nmap("<leader>L", ":LspRestart<CR>", "LSP Restart")
+      nmap("<leader>r", vim.lsp.buf.rename, "Rename")
+      nmap("gD", vim.lsp.buf.declaration, "Go to declaration")
+      nmap("gd", vim.lsp.buf.definition, "Go to definition")
+      nmap("gR", require("telescope.builtin").lsp_references, "Get references in a telescope list")
+      nmap("gr", function()
+        vim.lsp.buf.references(nil, {
+          on_list = function(options)
+            vim.fn.setqflist({}, " ", options)
+            vim.api.nvim_command("cfirst")
+          end,
+        })
+      end, "Get references in a quickfix list")
+      nmap("gI", vim.lsp.buf.implementation, "Go to implementation")
+      nmap("gt", vim.lsp.buf.type_definition, "Go to type definition")
+      nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+      nmap("H", vim.lsp.buf.signature_help, "Signature Help")
+      nmap("<leader>cf", vim.lsp.buf.format, "Format file")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "Code action")
+      nmap("<leader>L", ":LspRestart<CR>", "LSP Restart")
+    end
+
+    local servers = {
+      gopls = {},
+      terraformls = {
+        cmd = { "terraform-ls", "serve" },
+      },
+      ruff = {},
+      pyright = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            typeCheckingMode = "off",
+            useLibraryCodeForTypes = true,
+            diagnosticMode = "workspace",
+          },
+        },
+      },
+      rust_analyzer = {},
+      html = {},
+      sqlls = {},
+      helm_ls = {},
+      zls = {},
+      golangci_lint_ls = {},
+      clangd = {},
+      lua_ls = {
+        Lua = {
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+        },
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+      },
+    }
+
+    -- tfvars bugfix for terraform-ls. This is a temporary fix for neovim 0.9.0
+    vim.api.nvim_create_autocmd("BufReadPost", {
+      pattern = "*.tfvars,*.tf",
+      callback = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        vim.api.nvim_buf_set_option(bufnr, "filetype", "terraform")
+      end,
+    })
+
+    -- treat all files with the extension .tmp as the filetype of the extension
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = "*.tmp",
+      callback = function(event)
+        local original_name = vim.fn.fnamemodify(event.file, ":r")
+        local filetype = vim.filetype.match({ filename = original_name })
+        if filetype then
+          vim.bo.filetype = filetype
+        end
+      end,
+    })
+
+    require("neodev").setup()
+
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "",
+          package_pending = "",
+          package_uninstalled = "",
+        },
+      },
+    })
+
+    local null_ls = require("null-ls")
+
+    local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
+    local stylua_root_files = { "stylua.toml", ".stylua.toml" }
+
+    local root_has_file = function(files)
+      return function(utils)
+        return utils.root_has_file(files)
+      end
+    end
+
+    local opts = {
+      prettier_formatting = {
+        condition = root_has_file(prettier_root_files),
+      },
+      stylua_formatting = {
+        condition = root_has_file(stylua_root_files),
+      },
+    }
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettier.with(opts.prettier_formatting),
+        null_ls.builtins.formatting.stylua.with(opts.stylua_formatting),
+        null_ls.builtins.formatting.clang_format,
+      },
+    })
+
+    local mason_lspconfig = require("mason-lspconfig")
+
+    mason_lspconfig.setup({
+      automatic_installation = true,
+      ensure_installed = vim.tbl_keys(servers),
+    })
+
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = servers[server_name],
+        })
+      end,
+    })
+
+    -- auto format
+    local format_is_enabled = true
+    vim.api.nvim_create_user_command("ToggleAutoFormat", function()
+      format_is_enabled = not format_is_enabled
+      print("Setting autoformatting to: " .. tostring(format_is_enabled))
+    end, {})
+
+    local _augroups = {}
+    local get_augroup = function(client)
+      if not _augroups[client.id] then
+        local group_name = "kickstart-lsp-format-" .. client.name
+        local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+        _augroups[client.id] = id
+      end
+
+      return _augroups[client.id]
+    end
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach-format", { clear = true }),
+      callback = function(args)
+        local client_id = args.data.client_id
+        local client = vim.lsp.get_client_by_id(client_id)
+        local bufnr = args.buf
+
+        if client ~= nil and not client.server_capabilities.documentFormattingProvider then
+          return
         end
 
-        local servers = {
-            gopls = {},
-            terraformls = {
-                cmd = { "terraform-ls", "serve" },
-            },
-            ruff = {},
-            pyright = {
-                python = {
-                    analysis = {
-                        autoSearchPaths = true,
-                        typeCheckingMode = "off",
-                        useLibraryCodeForTypes = true,
-                        diagnosticMode = "workspace",
-                    },
-                },
-            },
-            rust_analyzer = {},
-            html = {},
-            sqlls = {},
-            helm_ls = {},
-            zls = {},
-            golangci_lint_ls = {},
-            clangd = {},
-            lua_ls = {
-                Lua = {
-                    workspace = { checkThirdParty = false },
-                    telemetry = { enable = false },
-                },
-            },
-        }
-
-        -- tfvars bugfix for terraform-ls. This is a temporary fix for neovim 0.9.0
-        vim.api.nvim_create_autocmd("BufReadPost", {
-            pattern = "*.tfvars,*.tf",
-            callback = function()
-                local bufnr = vim.api.nvim_get_current_buf()
-                vim.api.nvim_buf_set_option(bufnr, "filetype", "terraform")
-            end,
-        })
-
-        -- treat all files with the extension .tmp as the filetype of the extension
-        vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-            pattern = "*.tmp",
-            callback = function(event)
-                local original_name = vim.fn.fnamemodify(event.file, ":r")
-                local filetype = vim.filetype.match({ filename = original_name })
-                if filetype then
-                    vim.bo.filetype = filetype
-                end
-            end,
-        })
-
-        require("neodev").setup()
-
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-        require("mason").setup({
-            ui = {
-                border = "rounded",
-                icons = {
-                    package_installed = "",
-                    package_pending = "",
-                    package_uninstalled = "",
-                },
-            },
-        })
-
-        local null_ls = require("null-ls")
-
-        local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
-        local stylua_root_files = { "stylua.toml", ".stylua.toml" }
-
-        local root_has_file = function(files)
-            return function(utils)
-                return utils.root_has_file(files)
-            end
-        end
-
-        local opts = {
-            prettier_formatting = {
-                condition = root_has_file(prettier_root_files),
-            },
-            stylua_formatting = {
-                condition = root_has_file(stylua_root_files),
-            },
-        }
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.prettier.with(opts.prettier_formatting),
-                null_ls.builtins.formatting.stylua.with(opts.stylua_formatting),
-                null_ls.builtins.formatting.clang_format,
-            },
-        })
-
-        local mason_lspconfig = require("mason-lspconfig")
-
-        mason_lspconfig.setup({
-            automatic_installation = true,
-            ensure_installed = vim.tbl_keys(servers),
-        })
-
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                    settings = servers[server_name],
-                })
-            end,
-        })
-
-        -- auto format
-        local format_is_enabled = true
-        vim.api.nvim_create_user_command("ToggleAutoFormat", function()
-            format_is_enabled = not format_is_enabled
-            print("Setting autoformatting to: " .. tostring(format_is_enabled))
-        end, {})
-
-        local _augroups = {}
-        local get_augroup = function(client)
-            if not _augroups[client.id] then
-                local group_name = "kickstart-lsp-format-" .. client.name
-                local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-                _augroups[client.id] = id
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = get_augroup(client),
+          buffer = bufnr,
+          callback = function()
+            if not format_is_enabled then
+              return
             end
 
-            return _augroups[client.id]
-        end
-
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("kickstart-lsp-attach-format", { clear = true }),
-            callback = function(args)
-                local client_id = args.data.client_id
-                local client = vim.lsp.get_client_by_id(client_id)
-                local bufnr = args.buf
-
-                if client ~= nil and not client.server_capabilities.documentFormattingProvider then
-                    return
-                end
-
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = get_augroup(client),
-                    buffer = bufnr,
-                    callback = function()
-                        if not format_is_enabled then
-                            return
-                        end
-
-                        vim.lsp.buf.format({
-                            async = false,
-                            filter = function(c)
-                                return c.id == client.id
-                            end,
-                        })
-                    end,
-                })
-            end,
+            vim.lsp.buf.format({
+              async = false,
+              filter = function(c)
+                return c.id == client.id
+              end,
+            })
+          end,
         })
-    end,
+      end,
+    })
+  end,
 }
