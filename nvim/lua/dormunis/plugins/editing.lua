4306e849960c393ed4d775163bfcdc814dec5cea
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-01-12 19:54:24 +0200

    some editing changes

--- nvim/lua/dormunis/plugins/editing.lua
+++ nvim/lua/dormunis/plugins/editing.lua
@@ -1,137 +1,105 @@
-local cmp_providers = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' }
 return {
-    {
-        "preservim/vimux",
-        lazy = false,
+  {
+    "preservim/vimux",
+    lazy = false,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end
+  },
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = 'Undo Tree' })
+    end
+  },
+  {
+    'saghen/blink.cmp',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'echasnovski/mini.icons',
     },
-    {
-        "kylechui/nvim-surround",
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup()
-        end
-    },
-    {
-        'mbbill/undotree',
-        config = function()
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = 'Undo Tree' })
-        end
-    },
-    {
-        'saghen/blink.cmp',
-        dependencies = {
-            'rafamadriz/friendly-snippets',
-            'giuxtaposition/blink-cmp-copilot',
-            'echasnovski/mini.icons',
+    version = '*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = true,
+            auto_insert = false,
+          },
         },
-        version = '*',
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
-        opts = {
-            completion = {
-                documentation = {
-                    auto_show = true,
-                    auto_show_delay_ms = 500,
-                },
-                menu = {
-                    auto_show = false,
-                    draw = {
-                        components = {
-                            kind_icon = {
-                                ellipsis = false,
-                                text = function(ctx)
-                                    local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return kind_icon
-                                end,
-                                highlight = function(ctx)
-                                    local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return hl
-                                end,
-                            }
-                        }
-                    }
-                }
-            },
-            keymap = {
-                preset = 'default',
-                ['<C-space>'] = { function(cmp) cmp.show({ providers = cmp_providers }) end },
-                ['<C-n>'] = { function(cmp)
-                    vim.schedule(function() require('blink.cmp.completion.list').select_next({ auto_insert = true }) end)
-                end },
-                ['<C-p>'] = { function(cmp)
-                    vim.schedule(function() require('blink.cmp.completion.list').select_prev({ auto_insert = true }) end)
-                end },
-            },
-            appearance = {
-                nerd_font_variant = 'mono',
-                kind_icons = {
-                    Copilot = "",
-                    Text = '󰉿',
-                    Method = '󰊕',
-                    Function = '󰊕',
-                    Constructor = '󰒓',
-
-                    Field = '󰜢',
-                    Variable = '󰆦',
-                    Property = '󰖷',
-
-                    Class = '󱡠',
-                    Interface = '󱡠',
-                    Struct = '󱡠',
-                    Module = '󰅩',
-
-                    Unit = '󰪚',
-                    Value = '󰦨',
-                    Enum = '󰦨',
-                    EnumMember = '󰦨',
-
-                    Keyword = '󰻾',
-                    Constant = '󰏿',
-
-                    Snippet = '󱄽',
-                    Color = '󰏘',
-                    File = '󰈔',
-                    Reference = '󰬲',
-                    Folder = '󰉋',
-                    Event = '󱐋',
-                    Operator = '󰪚',
-                    TypeParameter = '󰬛',
-                },
-            },
-            sources = {
-                default = cmp_providers,
-                cmdline = {},
-                providers = {
-                    copilot = {
-                        name = "copilot",
-                        module = "blink-cmp-copilot",
-                        score_offset = 100,
-                        async = true,
-                        transform_items = function(_, items)
-                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-                            local kind_idx = #CompletionItemKind + 1
-                            CompletionItemKind[kind_idx] = "Copilot"
-                            for _, item in ipairs(items) do
-                                item.kind = kind_idx
-                            end
-                            return items
-                        end,
-                    },
-                },
-
-            },
-            signature = { enabled = true }
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
         },
-        opts_extend = { "sources.default" }
+        menu = {
+          auto_show = false,
+          draw = {
+            components = {
+              kind_icon = {
+                ellipsis = false,
+                text = function(ctx)
+                  local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                  return kind_icon
+                end,
+                highlight = function(ctx)
+                  local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                  return hl
+                end,
+              }
+            }
+          }
+        }
+      },
+      keymap = {
+        preset = 'default',
+        ['<C-space>'] = {},
+        ['<C-m>'] = { function(cmp)
+          if cmp.is_visible() then
+            cmp.cancel({})
+            cmp.hide({})
+          else
+            cmp.show({ providers = { 'lsp', 'path', 'snippets', 'buffer' } })
+          end
+        end },
+        ['<C-n>'] = { function(cmp)
+          vim.schedule(function() require('blink.cmp.completion.list').select_next({ auto_insert = true }) end)
+        end },
+        ['<C-p>'] = { function(cmp)
+          vim.schedule(function() require('blink.cmp.completion.list').select_prev({ auto_insert = true }) end)
+        end },
+      },
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+      sources = {
+        default = { "buffer", "lsp" },
+        cmdline = {},
+      },
+      signature = { enabled = true }
     },
-    {
-        "zbirenbaum/copilot.lua",
-        config = function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    }
+    opts_extend = { "sources.default" }
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({
+        panel = { enabled = false },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-space>",
+            dismiss = "Esc",
+          },
+        },
+      })
+    end,
+  }
 }
