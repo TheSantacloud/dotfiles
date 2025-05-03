ab28a8a85d76c7a1551d68f436ae73d8378b0928
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-05-03 10:19:02 +0300

    folke what did you do to trouble

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,19 +4,20 @@ require("dormunis.globals")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.keymap.set("n", "<leader>sl", function()
-    require("lazy").home()
+  require("lazy").home()
 end, { desc = "Open Lazy" })
 
+require("dormunis.diagnostics").setup()
 require("lazy").setup("dormunis.plugins")
