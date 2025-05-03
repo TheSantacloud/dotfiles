32793e3d5ca136f71a5c363bc8d5298478e9d54e
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-05-03 11:28:44 +0300

    yes

--- nvim/lua/dormunis/plugins/mini.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'echasnovski/mini.nvim',
-    version = "*",
-    config = function()
-        local statusline = require("mini.statusline")
-        statusline.setup({ use_icons = true })
-    end
-}
