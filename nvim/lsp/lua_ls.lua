050ae2ba446376b13dc6563c816a30b3a32102d5
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-05-09 13:44:39 +0300

    new lsp support 0.11 neovim

---
+++ nvim/lsp/lua_ls.lua
@@ -0,0 +1,16 @@
+return {
+  settings = {
+    Lua = {
+      workspace = {
+        checkThirdParty = false,
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
