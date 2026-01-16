61512bdb3bf68ca759159ef175a5ddf9e5df889a
Dor Munis <5811812+dormunis@users.noreply.github.com>
2026-01-16 23:13:49 +0200

    merge stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require("dormunis.options")
 require("dormunis.keymap")
 require("dormunis.globals")
+require("dormunis.git")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
